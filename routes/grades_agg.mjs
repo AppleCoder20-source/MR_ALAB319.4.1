054312e0c5230d4131507204c434d529a6a2a8de
Mashiur <56059603+AppleCoder20-source@users.noreply.github.com>
2024-12-01 23:07:38 -0500

    Finished LAB 4.1 and added schema validation

--- routes/grades_agg.mjs
+++ routes/grades_agg.mjs
@@ -5,7 +5,7 @@ import { ObjectId } from "mongodb";
 const router = express.Router();
 
 /**
- * It is not best practice to seperate these routes
+ * It is not best practice to separate these routes
  * like we have done here. This file was created
  * specifically for educational purposes, to contain
  * all aggregation routes in one place.
@@ -14,12 +14,98 @@ const router = express.Router();
 /**
  * Grading Weights by Score Type:
  * - Exams: 50%
- * - Quizes: 30%
+ * - Quizzes: 30%
  * - Homework: 20%
  */
+    // Create indexes
+    await db.collection("grades").createIndex({ class_id: 1 });
+    await db.collection("grades").createIndex({ learner_id: 1 });
+    // Compound index
+    await db.collection("grades").createIndex({
+      class_id: 1,
+      learner_id: 1,
+    });
+
+    const schemaValidator = {
+      $jsonSchema: {
+        bsonType: "object",
+        required: ["class_id", "learner_id"],
+        properties: {
+          class_id: {
+            bsonType: "number",
+            minimum: 0,
+            maximum: 300,
+            description: "must be a number between 0 and 300",
+          },
+          learner_id: {
+            bsonType: "number", 
+            minimum: 0,
+            description: "must be a number greater than 0",
+          },
+          scores: {
+            bsonType: "array",
+            items: {
+              bsonType: "object",
+              required: ["type", "score"],
+              properties: {
+                type: {
+                  bsonType: "string",
+                  enum: ["exam", "quiz", "homework"],
+                  description: "must be 'exam', 'quiz', or 'homework'",
+                },
+                score: {
+                  bsonType: "number",
+                  minimum: 0,
+                  maximum: 100,
+                  description: "must be a number between 0 and 100",
+                },
+              },
+            },
+          },
+        },
+      },
+    };
+    await db.command({
+      collMod: "grades",
+      validator: schemaValidator,
+      validationLevel: "strict",
+      validationAction: "error",
+    });
+    console.log("Indexes and schema validation created successfully.");
+
+// Testing our schema validation here
+async function Tests (){
+  try {
+    await db.collection("grades").insertMany([
+      {
+        class_id: 2,
+        learner_id: 2,
+        scores: [
+          { type: "exam", score: 80 },
+          { type: "quiz", score: 70 },
+          { type: "homework", score: 90 },
+          { type: "homework", score: 100 },
+        ],
+      },
+     /* { Uncommenting this would make schema test fail and it will not insert anything
+        class_id: "abc",
+        learner_id: "aa",
+        scores: [
+          { type: "exam", score: 85 },
+          { type: "quiz", score: 75 },
+          { type: "homework", score: 95 },
+          { type: "homework", score: 88 },
+        ],
+      },*/ 
+    ]);
+    console.log("Insert successful.");
+  } catch (err) {
+    console.error("Insert failed due to schema validation:", err);
+  }
+} Tests();
+
 router.get("/learner/:id/avg-class", async (req, res) => {
-    let collection = await db.collection("grades");
-  
+    let collection = db.collection("grades");
     let result = await collection
       .aggregate([
         {
@@ -33,29 +119,29 @@ router.get("/learner/:id/avg-class", async (req, res) => {
             _id: "$class_id",
             quiz: {
               $push: {
-                $cond: {
-                  if: { $eq: ["$scores.type", "quiz"] },
-                  then: "$scores.score",
-                  else: "$$REMOVE",
-                },
+                $cond: [
+                  { $eq: ["$scores.type", "quiz"] },
+                  "$scores.score",
+                  "$$REMOVE",
+                ],
               },
             },
             exam: {
               $push: {
-                $cond: {
-                  if: { $eq: ["$scores.type", "exam"] },
-                  then: "$scores.score",
-                  else: "$$REMOVE",
-                },
+                $cond: [
+                  { $eq: ["$scores.type", "exam"] },
+                  "$scores.score",
+                  "$$REMOVE",
+                ],
               },
             },
             homework: {
               $push: {
-                $cond: {
-                  if: { $eq: ["$scores.type", "homework"] },
-                  then: "$scores.score",
-                  else: "$$REMOVE",
-                },
+                $cond: [
+                  { $eq: ["$scores.type", "homework"] },
+                  "$scores.score",
+                  "$$REMOVE",
+                ],
               },
             },
           },
@@ -75,154 +161,152 @@ router.get("/learner/:id/avg-class", async (req, res) => {
         },
       ])
       .toArray();
-  
-    if (!result) res.send("Not found").status(404);
-    else res.send(result).status(200);
-  });
-
+    
+      if (!result) res.send("Not found").status(404);
+      else res.send({learners,percentage,totalLearners}).status(200);
+});
 
 router.get("/stats", async (req, res) => {
-  let collection = await db.collection("grades");
-
-  let result = await collection
-    .aggregate([
-      {
-        $unwind: { path: "$scores" },
-      },
-      {
-        $group: {
-          _id: "$learner_id",
-          quiz: {
-            $push: {
-              $cond: {
-                if: { $eq: ["$scores.type", "quiz"] },
-                then: "$scores.score",
-                else: "$$REMOVE",
+    let collection = db.collection("grades");
+    let result = await collection
+      .aggregate([
+        {
+          $unwind: { path: "$scores" },
+        },
+        {
+          $group: {
+            _id: "$learner_id",
+            quiz: {
+              $push: {
+                $cond: [
+                  { $eq: ["$scores.type", "quiz"] },
+                  "$scores.score",
+                  "$$REMOVE",
+                ],
               },
             },
-          },
-          exam: {
-            $push: {
-              $cond: {
-                if: { $eq: ["$scores.type", "exam"] },
-                then: "$scores.score",
-                else: "$$REMOVE",
+            exam: {
+              $push: {
+                $cond: [
+                  { $eq: ["$scores.type", "exam"] },
+                  "$scores.score",
+                  "$$REMOVE",
+                ],
               },
             },
-          },
-          homework: {
-            $push: {
-              $cond: {
-                if: { $eq: ["$scores.type", "homework"] },
-                then: "$scores.score",
-                else: "$$REMOVE",
+            homework: {
+              $push: {
+                $cond: [
+                  { $eq: ["$scores.type", "homework"] },
+                  "$scores.score",
+                  "$$REMOVE",
+                ],
               },
             },
           },
         },
-      },
-      {
-        $project: {
-          _id: 1,
-          avg: {
-            $sum: [
-              { $multiply: [{ $avg: "$exam" }, 0.5] },
-              { $multiply: [{ $avg: "$quiz" }, 0.3] },
-              { $multiply: [{ $avg: "$homework" }, 0.2] },
-            ],
+        {
+          $project: {
+            _id: 1,
+            avg: {
+              $sum: [
+                { $multiply: [{ $avg: "$exam" }, 0.5] },
+                { $multiply: [{ $avg: "$quiz" }, 0.3] },
+                { $multiply: [{ $avg: "$homework" }, 0.2] },
+              ],
+            },
           },
         },
-      },
-     {
-        $match:{
-         avg: {$gte:70},
-       },
-      },
-    ])
-    .toArray();
-  console.log(result)
+        {
+          $match: {
+            avg: { $gte: 70 },
+          },
+        },
+      ])
+      .toArray();
 
-  const totalLearners = (await collection.distinct("learner_id")).length;
-  const percentage = (result.length / totalLearners) * 100;
-  const learners = result.length;
+    const totalLearners = (await collection.distinct("learner_id")).length;
+    const percentage = (result.length / totalLearners) * 100;
+    const learners = result.length;
 
-  console.log(totalLearners)
-
-  if (!result) res.send("Not found").status(404);
-  else res.send({learners,percentage,totalLearners}).status(200);
+    if (!result) res.send("Not found").status(404);
+    else res.send({totalLearners,learners,percentage}).status(200);
+ 
 });
-router.get("/stats/:id", async (req, res) => {
-  let collection = await db.collection("grades");
-  let classId = Number(req.params.id);
 
-  let result = await collection
-    .aggregate([
-      {
-        $match: { class_id: classId },
-      },
-      {
-        $unwind: { path: "$scores" },
-      },
-      {
-        $group: {
-          _id: "$learner_id",
-          quiz: {
-            $push: {
-              $cond: {
-                if: { $eq: ["$scores.type", "quiz"] },
-                then: "$scores.score",
-                else: "$$REMOVE",
+router.get("/stats/:id", async (req, res) => {
+    let collection = db.collection("grades");
+    let class_Id = Number(req.params.id);
+
+    let result = await collection
+      .aggregate([
+        {
+          $match: { class_id: class_Id },
+        },
+        {
+          $unwind: { path: "$scores" },
+        },
+        {
+          $group: {
+            _id: "$learner_id",
+            quiz: {
+              $push: {
+                $cond: [
+                  { $eq: ["$scores.type", "quiz"] },
+                  "$scores.score",
+                  "$$REMOVE",
+                ],
               },
             },
-          },
-          exam: {
-            $push: {
-              $cond: {
-                if: { $eq: ["$scores.type", "exam"] },
-                then: "$scores.score",
-                else: "$$REMOVE",
+            exam: {
+              $push: {
+                $cond: [
+                  { $eq: ["$scores.type", "exam"] },
+                  "$scores.score",
+                  "$$REMOVE",
+                ],
               },
             },
-          },
-          homework: {
-            $push: {
-              $cond: {
-                if: { $eq: ["$scores.type", "homework"] },
-                then: "$scores.score",
-                else: "$$REMOVE",
+            homework: {
+              $push: {
+                $cond: [
+                  { $eq: ["$scores.type", "homework"] },
+                  "$scores.score",
+                  "$$REMOVE",
+                ],
               },
             },
           },
         },
-      },
-      {
-        $project: {
-          _id: 1,
-          avg: {
-            $sum: [
-              { $multiply: [{ $avg: "$exam" }, 0.5] },
-              { $multiply: [{ $avg: "$quiz" }, 0.3] },
-              { $multiply: [{ $avg: "$homework" }, 0.2] },
-            ],
+        {
+          $project: {
+            _id: 1,
+            avg: {
+              $sum: [
+                { $multiply: [{ $avg: "$exam" }, 0.5] },
+                { $multiply: [{ $avg: "$quiz" }, 0.3] },
+                { $multiply: [{ $avg: "$homework" }, 0.2] },
+              ],
+            },
           },
         },
-      },
-      {
-        $match: {
-          avg: { $gte: 70 },
+        {
+          $match: {
+            avg: { $gte: 70 },
+          },
         },
-      },
-    ])
-    .toArray();
+      ])
+      .toArray();
 
     const totalLearners = (
-    await collection.distinct("class_id")).length;
+      await collection.distinct("learner_id", { class_id: class_Id })
+    ).length;
+    const learners = result.length;
+    const percentage = (learners / totalLearners) * 100;
 
-  const learners = result.length;
-  const percentage = (learners / totalLearners) * 100;
+    if (!result) res.send("Not found").status(404);
+    else res.send({totalLearners,learners,percentage}).status(200);
 
-  if (!result) res.send("Not found").status(404);
-  else res.send({totalLearners,learners,percentage}).status(200);
 });
 
 export default router;
